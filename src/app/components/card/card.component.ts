29c66caf0087074a95c65d2a814f279895572c9f
Kinga Jaranowska <kobietazkosmosu@gmail.com>
2024-11-18 11:46:16 +0100

    A bit of styling

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,15 +1,17 @@
 import {Component, Input} from '@angular/core';
 import {MatCard, MatCardContent, MatCardHeader} from '@angular/material/card';
 import {CardInfo} from '../../models/response.interface';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [MatCard, MatCardContent, MatCardHeader],
+  imports: [MatCard, MatCardContent, MatCardHeader, NgForOf],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
 export class CardComponent {
   @Input({required:true}) cardInfo!:CardInfo;
 
+  protected readonly Object = Object;
 }
